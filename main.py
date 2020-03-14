fc3d19a08a8afa7e4d67db04be8813183e8c395f
SjonDeBok <56730503+SjonDeBok@users.noreply.github.com>
2020-03-14 17:12:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+def euler_five(limit=20):
+  """
+  Calculates the smallest positive number that is evenly divisible by all of the numbers from 1 to limit value.
+  Returns: int
+  """
+  prime = primeGen()
+  num = next(prime) 
+  result = 1
+  while num <= limit:
+    result *= highestPowerUpTo(num, limit)
+    num = next(prime)
+  return result
+
+
 def primeGen():
   """
   Generator of prime numbers
   Returns: int
   """
-  yield 2
+  yield 2 # The smallest prime number
   primes = []
-  nextprime = 3
+  nextprime = 3 # The smallest odd prime number
   while True:
     primes.append(nextprime)
     yield nextprime
     while True:
-      nextprime += 2
+      nextprime += 2 # All other prime numbers are odd, even numbers need not be tested
       remainders = map(lambda x: nextprime%x, primes)
       if not 0 in remainders: 
         break
@@ -19,7 +33,7 @@ def primeGen():
 def highestPowerUpTo(num, limit):
   """
   Calculates the highest power of num up to the limit value
-  e.g. if num = 2 and limit = 20: it calculates 2^4 = 16 as the highest power of 2 up to 20 
+  e.g. if num = 2 and limit = 20: it calculates 2**4 = 16 as the highest power of 2 up to 20 
   Returns: int
   """
   powered = num
@@ -28,14 +42,4 @@ def highestPowerUpTo(num, limit):
   return int(powered/num)
 
 
-limit = 20
-prime = primeGen()
-powers = []
-num = next(prime) 
-while num <= limit:
-  powers.append(highestPowerUpTo(num, limit))
-  num = next(prime) 
-result = 1
-for num in powers:
-  result *= num
-print(result)
+print(euler_five())
